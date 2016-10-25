d819387d46b87b9ab88ec8a98cb87b1b5dc3120d
Omer <10bitomaroof@seecs.edu.pk>
2016-10-25 18:27:29 +0200

    Added Skelton Socket Connection Wrapper

---
+++ app/src/main/java/com/semanticweb/receipe/receipeapp/SocketConnectionWrapper.java
@@ -0,0 +1,22 @@
+package com.semanticweb.receipe.receipeapp;
+
+import android.util.Log;
+
+import com.koushikdutta.async.http.BasicNameValuePair;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Omer on 10/25/2016.
+ */
+
+public class SocketConnectionWrapper {
+
+    public SocketConnectionWrapper(){
+
+    }
+}
