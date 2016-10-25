a832aef0e58daa4cd5c21c8e7955a827853eeb36
Omer <10bitomaroof@seecs.edu.pk>
2016-10-25 18:47:00 +0200

    Added  Socket Connection Wrapper

--- app/src/main/java/com/semanticweb/receipe/receipeapp/SocketConnectionWrapper.java
+++ app/src/main/java/com/semanticweb/receipe/receipeapp/SocketConnectionWrapper.java
@@ -1,10 +1,14 @@
 package com.semanticweb.receipe.receipeapp;
 
+import android.os.Build;
 import android.util.Log;
 
-import com.koushikdutta.async.http.BasicNameValuePair;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,7 +20,57 @@ import static android.content.ContentValues.TAG;
 
 public class SocketConnectionWrapper {
 
+    //webSocket
+    static private WebSocketClient mWebSocketClient;
+
     public SocketConnectionWrapper(){
 
     }
+
+
+    /**
+     *Maintains connection with websocket and Life Cycle of WebSocket
+     */
+    private void connectToWebSocket(String urlString){
+        URI uri;
+        try {
+            uri = new URI(urlString);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                //Message is received update UI
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
+    }
+
+    /**
+     *Helper Methods to interact with WebSocket
+     */
+    public static void sendMessage(String message){
+        mWebSocketClient.send(message);
+    }
+
 }
